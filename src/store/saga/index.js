fc35170c28d7f16aac8dc6d24a7c87966e6b3764
VinhPham <vpham@star.global>
2022-06-26 23:42:52 +0700

    refactor: Tidy up saga root logic

--- src/store/saga/index.js
+++ src/store/saga/index.js
@@ -1,9 +1,9 @@
-import { fork } from 'redux-saga/effects';
+import {
+	all,
+	fork
+} from 'redux-saga/effects';
 import todoSaga from './todo.saga';
 
 export default function* rootSaga() {
-	yield fork(todoSaga[0]);
-	yield fork(todoSaga[1]);
-	yield fork(todoSaga[2]);
-	yield fork(todoSaga[3]);
+	yield all(todoSaga.map(saga => fork(saga)));
 }
